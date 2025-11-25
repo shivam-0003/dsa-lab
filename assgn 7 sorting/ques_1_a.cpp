5f8786cdc58c2ee18597d12e0f24f9cea2c4c823
Shivam Singh <shivamsingh289592@gmail.com>
2025-11-25 16:02:03 +0530

    Add files via upload

---
+++ assgn 7 sorting/ques_1_a.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+void selectionsort(int arr[], int n)
+{
+    int i;
+    int min;
+
+    for(int i=0;i<n-1;i++)
+    {
+      int min=i;
+      
+      for (int j=i+1;j<n;j++)
+      {
+        if (arr[j]<arr[min])
+        min=j;
+      }
+      swap(arr[i],arr[min]);
+
+    }
+}
+
+int main(){
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    selectionsort(arr, n);
+
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
